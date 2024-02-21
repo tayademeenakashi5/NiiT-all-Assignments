efc9c7a9eddd43b864d4018e3f78fced5ccb3762
Meenakshi Tayade <85633773+tayademeenakashi5@users.noreply.github.com>
2024-02-21 20:43:05 +0530

    Add files via upload

---
+++ C1-Programming foundation using java/S2-Intro to java,Variables,Datatypes and operators/Practice/EnquiryForm.java
@@ -0,0 +1,24 @@
+class EnquiryForm {
+    public static void main(String[] args) {
+		String name ="john";
+		long contactNumber = 412312556L ;
+		String email ="john@gmail.com";
+		int age = 35;
+		char gender = 'm';
+		String occupation = "Manager";
+		double annualSalary =10000;
+		String nationality = "Indian";
+		boolean isMarried = true;
+        System.out.println(name);
+		System.out.println("The number :" +contactNumber);
+		System.out.println("The email :" +email);
+		System.out.println("The age :" +age);
+        System.out.println("The gender :" +gender);
+		System.out.println("The occupation :" +occupation);
+		System.out.println("The salary :" +annualSalary);
+		System.out.println("The nationality :" +nationality);
+		System.out.println("The Married :" +isMarried);
+	}
+    
+}
+
---
+++ C1-Programming foundation using java/S2-Intro to java,Variables,Datatypes and operators/Practice/SellingPrice.java
@@ -0,0 +1,11 @@
+
+class SellingPrice{
+	 public static void main(String[] args) {
+		String ItemName = "Kaisa Bowl";
+		double SellingPrice = 10.99 ;
+		double hike = 0.05*10.99;
+		double SellingPriceAmount = SellingPrice+hike;
+		System.out.println("Revised Selling price for Rounded Kaisa Bowl is : $" +SellingPriceAmount);
+    }
+}
+
